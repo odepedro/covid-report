8e4c87e7b570fd1d1d0396ab2a110a74904e384c
odepedro <odepedrorodas@gmail.com>
2022-02-23 16:32:13 +0100

    Restoring files

---
+++ README.md
@@ -0,0 +1,13 @@
+# covid-report
+
+The python script 'report.py' downloads the latest data of OWID dataset from 'https://covid.ourworldindata.org/data/latest/owid-covid-latest.csv' and filters the information to create a custom report as CSV.
+
+Source repo:
+https://github.com/owid/covid-19-data
+
+# dependencies
+
+```
+sudo apt install python3-pip
+pip install pandas
+```
---
+++ report.py
@@ -0,0 +1,25 @@
+import requests, pandas as pd
+
+# vars
+src_filename = 'owid-covid-latest.csv'
+dst_filename = 'covid-report.csv'
+url = 'https://covid.ourworldindata.org/data/latest/' + src_filename
+
+# Define lists with the fields and required country names
+fieldnames = ['location', 'new_cases', 'total_cases', 'new_vaccinations', 'total_vaccinations']
+countrynames = ['Ukraine', 'Poland', 'Denmark', 'Switzerland', 'United States']
+
+# Download the latest data OWID dataset 
+r = requests.get(url, allow_redirects=True)
+open(src_filename, 'wb').write(r.content)
+
+# Read CSV specific columns
+df = pd.read_csv(src_filename, usecols = fieldnames)
+
+# Filter rows when matches one of the values on countrynames list
+boolean_series = df.location.isin(countrynames)
+filtered_df = df[boolean_series]
+
+# Write the output to dest_filename
+print(filtered_df)
+filtered_df.to_csv(dst_filename, sep=',', index=False) 
